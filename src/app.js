3378ae6cccdf0aa0fe53e8b86f76dbd4ed205d84
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-29 22:17:59 -0300

    criando rotas

--- src/app.js
+++ src/app.js
@@ -4,10 +4,26 @@ import express from 'express';
 // Criar uma instância do express
 const app = express();
 
+const personagens = [
+    {
+        id: 1,
+        nome: "Katniss Everdeen",
+    },
+    {
+        id: 2,
+        nome: "Peeta Mellark",
+    }
+];
+
 // req -> requisição (request)
 // res -> resposta (response)
 app.get("/", (req, res) => {
     res.status(200).send("Bem vindo a API Hunger Games");
+});
+
+app.get("/personagens", (req, res) => {
+    // Retornar a lista de personagens em formato JSON
+    res.status(200).json(personagens);
 });
 
 export default app;
