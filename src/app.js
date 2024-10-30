be1c07206bc0b89ec2756c10d616cf9ce786909c
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-29 22:50:30 -0300

    Aplicando Métodos HTTP

--- src/app.js
+++ src/app.js
@@ -17,17 +17,28 @@ const personagens = [
     }
 ];
 
+function getPersonagemById(id) {
+    return personagens.find(personagen => personagen.id === id);
+}
+
 // req -> requisição (request)
 // res -> resposta (response)
 
 // Rotas GET
+// Rota raiz
 app.get("/", (req, res) => {
     res.status(200).send("Bem vindo a API Hunger Games");
 });
-
+// Rota para listar todos os personagens
 app.get("/personagens", (req, res) => {
     // Retornar a lista de personagens em formato JSON
     res.status(200).json(personagens);
+});
+// Rota para listar um personagem específico
+app.get("/personagens/:id", (req, res) => {
+    const personagem = getPersonagemById(parseInt(req.params.id));
+    res.status(200).json(personagem);
+
 });
 
 // Rotas POST
@@ -36,5 +47,12 @@ app.post("/personagens", (req, res) => {
     personagens.push(req.body);
     res.status(201).send(`Personagem: ${req.body.nome} criado com sucesso`);
 });
+
+// Rotas PUT
+app.put("/personagens/:id", (req, res) => {
+    const personagem = getPersonagemById(parseInt(req.params.id));
+    personagem.nome = req.body.nome;
+    res.status(200).json(personagens);
+})
 
 export default app;
