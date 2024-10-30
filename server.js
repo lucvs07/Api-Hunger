53b9c939ff64c0d79265310215dd019e03375c9d
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-29 21:45:25 -0300

    Iniciando a criação de Rotas

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ import http from "http";
 
 const PORT = 3000; // Porta que o servidor irá escutar
 
+// Rotas da API
+const rotas  = {
+    "/" : "Bem vindo a API Hunger Games",
+    "/personagens" : "Lista de personagens",
+    "/daily" : "Detalhes do personagem diário",
+};
+
 // Criando um servidor http
 // req -> requisição (request)
 // res -> resposta (response)
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Api Hunger Games");
+    // req.url -> rota que o usuário está acessando
+    res.end(rotas[req.url] || "Rota não encontrada");
 })
 
 // Chamar o servidor para escutar a porta 3000
