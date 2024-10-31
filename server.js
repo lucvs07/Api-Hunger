0808933d0a943f78fecfc6639ac059031b356de1
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-10-31 19:11:54 -0300

    keep api alive

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import axios from "axios";
+import { set } from "mongoose";
+
 dotenv.config();
 
 const PORT = process.env.PORT; // Porta que o servidor irá escutar
@@ -9,3 +12,12 @@ const PORT = process.env.PORT; // Porta que o servidor irá escutar
 app.listen(PORT,() => {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
+
+// Função para manter api no ar
+const keepAlive = async () => {
+    axios.get(process.env.API)
+        .then(() => console.log("API no ar"))
+        .catch((error) => console.log("Erro ao manter API no ar: ", error.message));
+};
+
+setInterval(keepAlive, 7 * 60 * 1000); // Chamar a função a cada 13 minutos
